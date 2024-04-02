f4b4b7a0a1331ce476bc46a82f7bff4f7a51bb8a
jayrboy <j.jayox_@hotmail.com>
2024-04-02 19:34:35 +0700

    update

--- index.js
+++ index.js
@@ -8,15 +8,19 @@ import xhub from 'express-x-hub'
 import webhooks from './webhooks.js'
 
 const app = express()
-const port = process.env.PORT || 8000
 
-app.disable('x-powered-by')
 /* --- Middleware --- */
-app.use(morgan('dev'))
+if (process.env.NODE_ENV !== 'production') {
+  app.use(morgan('dev'))
+}
 app.use(cors())
 app.use(xhub({ algorithm: 'sha256', secret: process.env.APP_SECRET }))
 app.use(express.urlencoded({ extended: true })) // body-parser
 app.use(express.json()) // parser-json data sent in request.body
+
+app.get('/', (req, res) => {
+  res.send(`<h1>Hello From Server... <br> ${os.hostname()}</h1>`)
+})
 
 app.use('/webhooks', webhooks)
 
@@ -26,7 +30,12 @@ files.map(async (file) => {
   app.use('/api', fs.default)
 })
 
+app.use((req, res) => res.status(404).send('Route does not exist'))
+app.disable('x-powered-by')
+
 /* --- Server --- */
+const port = process.env.PORT || 8000
+
 app.listen(port, () =>
   console.log('Server running at http://localhost:%s', port)
 )

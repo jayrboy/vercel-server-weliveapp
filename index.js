3c28202171e1e21fe621a020e71cef9705756605
jayrboy <j.jayox_@hotmail.com>
2024-04-15 21:30:59 +0700

    simulate orders

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import xhub from 'express-x-hub'
 import cookieParser from 'cookie-parser'
 
 import webhooks from './webhooks.js'
+import { channel } from 'diagnostics_channel'
 
 const app = express()
 
@@ -34,6 +35,10 @@ files.map(async (file) => {
   app.use('/api', fs.default)
 })
 
+app.get('/o/:id', (req, res) => {
+  res.send(orderCustomer)
+})
+
 app.disable('x-powered-by')
 
 /* --- Server --- */
@@ -51,3 +56,110 @@ const startApp = () => {
 }
 
 startApp()
+
+let orderCustomer = [
+  {
+    _id: 1,
+    date_added: new Date(),
+    status: 'new',
+    channel: 'facebook',
+    total: 7801,
+    promotion: 0,
+    paid: false,
+    customer: [
+      {
+        id: '123_456',
+        name: 'Jay Jakkrit',
+        email: 'jay@test.com',
+        picture: {
+          data: {
+            url: 'http://m.gettywallpapers.com/wp-content/uploads/2023/06/Messi-pfp.jpg',
+          },
+        },
+      },
+    ],
+
+    orders: [
+      {
+        _id: 1,
+        code: 'A01',
+        name: 'กระเป๋าสัมภาระ',
+        quantity: 3,
+        price: 150,
+        total: 450,
+      },
+      {
+        _id: 2,
+        code: 'B02',
+        name: 'เสื้อยืด',
+        quantity: 5,
+        price: 299,
+        total: 1495,
+      },
+      {
+        _id: 3,
+        code: 'C03',
+        name: 'กางเกงยีนส์',
+        price: 499,
+        quantity: 2,
+        total: 998,
+      },
+      {
+        _id: 4,
+        code: 'D04',
+        name: 'รองเท้าผ้าใบ',
+        price: 899,
+        quantity: 1,
+        total: 899,
+      },
+      {
+        _id: 5,
+        code: 'E05',
+        name: 'หมวกกันน้ำ',
+        price: 149,
+        quantity: 4,
+        total: 596,
+      },
+      {
+        _id: 6,
+        code: 'F06',
+        name: 'ถุงเท้า',
+        price: 79,
+        quantity: 6,
+        total: 474,
+      },
+      {
+        _id: 7,
+        code: 'G07',
+        name: 'แว่นตากันแดด',
+        price: 150,
+        quantity: 2,
+        total: 300,
+      },
+      {
+        _id: 8,
+        code: 'H08',
+        name: 'กล้องถ่ายรูป',
+        price: 1999,
+        quantity: 1,
+        total: 1999,
+      },
+      {
+        _id: 9,
+        code: 'I09',
+        name: 'ปากกา',
+        price: 29,
+        quantity: 10,
+        total: 290,
+      },
+      {
+        _id: 10,
+        code: 'J10',
+        name: 'แผ่นรองเมาส์',
+        price: 100,
+        quantity: 3,
+        total: 300,
+      },
+    ],
+  },
+]

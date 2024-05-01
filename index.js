c1e974a654eeab894a9d04c6edb754b375bec71e
jayrboy <j.jayox_@hotmail.com>
2024-05-01 18:10:29 +0700

    add swagger

--- index.js
+++ index.js
@@ -4,58 +4,69 @@ import os from 'os'
 import { readdirSync } from 'fs'
 import morgan from 'morgan'
 import cors from 'cors'
-import xhub from 'express-x-hub'
+// import xhub from 'express-x-hub'
 import cookieParser from 'cookie-parser'
-
-import webhooks from './webhooks.js'
-import { channel } from 'diagnostics_channel'
+import swaggerUi from 'swagger-ui-express'
+import swaggerJsdoc from 'swagger-jsdoc'
+// import webhooks from './webhooks.js'
 
 const app = express()
+const files = readdirSync('./router')
 
 /* --- Middleware --- */
 if (process.env.NODE_ENV !== 'production') {
   app.use(morgan('dev'))
 }
+
 app.use(cors())
-app.use(xhub({ algorithm: 'sha256', secret: process.env.APP_SECRET }))
+// app.use(xhub({ algorithm: 'sha256', secret: process.env.APP_SECRET }))
 app.use(express.urlencoded({ extended: true })) // body-parser
 app.use(express.json()) // parser-json data sent in request.body
 
 app.use(cookieParser())
 
 app.get('/', (req, res) => {
-  res.send(`<h1>Hello From Server... <br> ${os.hostname()}</h1>`)
+  res.send(
+    `<h1>Server running at <br> ${os.hostname()}</h1> <br> <a href="/api-docs">Swagger API</a>`
+  )
 })
 
-app.use('/webhooks', webhooks)
-
-const files = readdirSync('./router')
 files.map(async (file) => {
   let fs = await import(`./router/${file}`)
   app.use('/api', fs.default)
 })
 
-app.get('/o/:id', (req, res) => {
-  res.send(orderCustomer)
-})
+const swaggerOption = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API Documentation',
+      version: '1.0.0',
+      description: 'A simple API documentation',
+    },
+    servers: [
+      {
+        url: 'http://localhost:8000',
+      },
+    ],
+  },
+  apis: ['./router/*.js'],
+}
+
+const swaggerSpec = swaggerJsdoc(swaggerOption)
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+
+// app.use('/webhooks', webhooks)
 
 app.disable('x-powered-by')
 
 /* --- Server --- */
 const port = process.env.PORT || 8000
 
-const startApp = () => {
-  try {
-    app.listen(port, () => {
-      console.log('Server running at http://localhost:%s', port)
-    })
-  } catch (error) {
-    console.log(error)
-    process.exit(1)
-  }
-}
-
-startApp()
+app.listen(port, () => {
+  console.log('Server running at http://localhost:%s', port)
+})
 
 let orderCustomer = [
   {
@@ -163,3 +174,7 @@ let orderCustomer = [
     ],
   },
 ]
+
+app.get('/o/:id', (req, res) => {
+  res.send(orderCustomer)
+})
